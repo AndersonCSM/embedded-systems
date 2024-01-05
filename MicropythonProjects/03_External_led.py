0a318a7220dc398ba83341e0be79164b1720d40b
AndersonCSM <96142983+AndersonCSM@users.noreply.github.com>
2024-01-05 19:15:12 -0300

    +

--- MicropythonProjects/03_External_led.py
+++ MicropythonProjects/03_External_led.py
@@ -3,12 +3,4 @@ import time
 
 led = Pin(16, Pin.OUT)
 
-try:
-    while True:
-        led.value(1)
-        time.sleep(0.5)
-        led.value(0)
-        time.sleep(0.5)
-except:
-    pass
-
+led.value(0)
