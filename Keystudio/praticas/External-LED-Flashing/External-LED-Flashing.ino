bbe8fcca025708de95ed8b434fbe3fbccca2c42c
Anderson Carlos <96142983+AndersonCSM@users.noreply.github.com>
2025-04-10 21:54:19 -0300

    +

---
+++ Keystudio/praticas/External-LED-Flashing/External-LED-Flashing.ino
@@ -0,0 +1,16 @@
+#define PIN_LED  20   //define the led pin
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED as an output.
+  pinMode(PIN_LED, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(PIN_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for 0.5s
+  digitalWrite(PIN_LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);                       // wait for 0.5s
+}
+//*************************************************************************************
--- MicropythonProjects/03_External_led.py
+++
@@ -1,6 +0,0 @@
-from machine import Pin
-import time
-
-led = Pin(16, Pin.OUT)
-
-led.value(0)
